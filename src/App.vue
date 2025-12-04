49413719f904d333b59b4cc2acd3b3edbedf025f
HossinAmin <hossinamin70@gmail.com>
2025-12-05 00:14:45 +0200

    init project

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex flex-col gap-5">
+    <h1 class="text-6xl text-red-500">Vue3 Swiper Playground</h1>
+  </div>
+</template>
+
+<style scoped></style>
